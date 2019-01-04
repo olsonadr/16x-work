fdfd56088264d5cd5a202aba16fc3e0c101dae2b
olsonadr <olsonnick10@yahoo.com>
2019-01-04 00:22:48 -0800

    Added move function and simple input, no rendering.

--- misc/fp/fpp.cpp
+++ misc/fp/fpp.cpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 #include <cmath>
+#include <ncurses.h>
+#include <sys/time.h>
 using namespace std;
 
 float ray(float, float, float, string, int);
@@ -9,16 +11,14 @@ void raycast_in_fov(float[], float, float, float, float, string, int, float, int
 bool is_wall(float, float, string, int);
 // int get_predominant_bearing(float);
 void set_steps(float &, float &, float);
+void render_view(float[], string, int, float, float);
+void move(int, float, float &, float &, float, string, int);
 
 int main()
 {
-    float player_x = 0.0f;
-    float player_y = 0.0f;
-    float player_a = 0.0f;
-
     int term_width = 100;
     int term_heigh = 30;
-    float * distances = new float[term_width];
+    float *distances = new float[term_width];
 
     string map = "";
     map += "####################";
@@ -35,25 +35,68 @@ int main()
     map += "#..................#";
     map += "#..................#";
     map += "#..................#";
-    map += "#..................#";
-    map += "#..................#";
-    map += "#..................#";
     map += "####################";
     int map_height = map.length() / map_width;
 
     // Tweakables
-    float view_distance = 10;
     float fov = 90;
+    float view_distance = 10;
+    float player_x = map_width / 2;
+    float player_y = map_height / 2;
+    float player_a = 0.0f;
+    float movement_distance = 0.1;
+    float sprinting_distance = 0.15;
     // End Tweakables
 
     // Game Loop
-    while (true)
-    {
+    char input;
+    bool keep_going = true;
+    while (keep_going)
+    {   
+        // User Input
+        input = getch();
+        switch (input)
+        {
+        case 'w':
+            move(0, movement_distance, player_x, player_y, player_a, map, map_width);
+            break;
+        case 'd':
+            move(1, movement_distance, player_x, player_y, player_a, map, map_width);
+            break;
+        case 's':
+            move(2, movement_distance, player_x, player_y, player_a, map, map_width);
+            break;
+        case 'a':
+            move(3, movement_distance, player_x, player_y, player_a, map, map_width);
+            break;
+        case 'W':
+            move(0, movement_distance, player_x, player_y, player_a, map, map_width);
+            break;
+        case 'D':
+            move(1, movement_distance, player_x, player_y, player_a, map, map_width);
+            break;
+        case 'S':
+            move(2, movement_distance, player_x, player_y, player_a, map, map_width);
+            break;
+        case 'A':
+            move(3, movement_distance, player_x, player_y, player_a, map, map_width);
+            break;
+        case 'q':
+            keep_going = false;
+        }
+        // End User Input
+
         raycast_in_fov(distances, player_x, player_y, player_a, view_distance, map, map_width, fov, term_width);
+        render_view(distances, map, map_width, player_x, player_y);
     }
+    // End Game Loop
 
     delete distances;
     return 0;
+}
+
+void render_view(float distances[], string map, int map_width, float player_x, float player_y)
+{
 }
 
 /*
@@ -102,6 +145,44 @@ float ray(float x, float y,
     }
 
     return 0.0;
+}
+
+// Moves the player in a passed direction (0,1,2,3 --> up, right, left down),
+//    unless that move would put them too close to a wall;
+void move(int direction, float distance,
+          float &player_x, float &player_y,
+          float player_angle, string map,
+          int map_width)
+{
+    bool valid_move = true;
+    float destination_x;
+    float destination_y;
+
+    switch (direction)
+    {
+    default:
+        set_steps(destination_x, destination_y, player_angle); // Step of dist 1 forward
+        break;
+    case 1:
+        set_steps(destination_x, destination_y, (player_angle + .25)); // Step of dist 1 right
+        break;
+    case 2:
+        set_steps(destination_x, destination_y, (player_angle + .5)); // Step of dist 1 forward
+        break;
+    case 3:
+        set_steps(destination_x, destination_y, (player_angle + .75)); // Step of distance 1 left
+        break;
+    }
+
+    destination_x = (destination_x / distance) + player_x;
+    destination_y = (destination_y / distance) + player_y;
+    valid_move = !(is_wall(destination_x, destination_y, map, map_width));
+
+    if (valid_move)
+    {
+        player_x = destination_x;
+        player_y = destination_y;
+    }
 }
 
 // Returns whether the given x and y are in a wall tile
